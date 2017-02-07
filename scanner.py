6f749498e378f83e1fb3d62c515936a110a99326
oelizondo <oscarmarcelo95@gmail.com>
2017-02-07 11:36:03 -0600

    fixes bug

--- scanner.py
+++ scanner.py
@@ -31,7 +31,6 @@ CNT = 121
 ALPHABET_CAP = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 ALPHABET_LOW = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 DIGITS       = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-CNTIFICADORES = ['A', 'E'
 
 #Matriz principal de estados
 #       &     |     >     <     =     @   Mayus   Minus  $     (      )   dig   _    .    ~   ,   raro  ESP
@@ -68,7 +67,7 @@ def filtro(c):
     elif c == '@':
         return 5
     elif c == '$':
-        return END - 100
+        return 8
     elif c == '(':
         return 9
     elif c == ')':
